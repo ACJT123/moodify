ac673fc98f0b4ca157b95dd89189c5175d91194d
ACJT123 <acjtacjt@gmail.com>
2024-08-28 20:50:55 +0800

    feat(theme): add theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export const palette = {
+  purple: "#585a79",
+  darkPurple: "#42435f",
+  milky: "#ffe4d3",
+  white: "#fff1ea",
+};
+
+export const theme = {
+  colors: {
+    actionSheet: palette.milky,
+    background: palette.purple,
+  },
+};
